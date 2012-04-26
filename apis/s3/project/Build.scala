a879e3b538fe1e4912543b6a60e328da1b08a16a
ewestra <ewestra@rhinofly.nl>
2012-04-26 16:12:34 +0200

    Fixed some settings

--- apis/s3/project/Build.scala
+++ apis/s3/project/Build.scala
@@ -4,16 +4,16 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "s3"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appName         = "api-s3"
+    val appVersion      = "1.0"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "utils" %% "utils" % "1.0-SNAPSHOT"
+      "nl.rhinofly" %% "api-utils" % "1.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+       organization := "nl.rhinofly"
     )
 
 }
