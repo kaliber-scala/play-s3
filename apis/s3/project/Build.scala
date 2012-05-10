24a20ca507c00e3473c151478344a01ccf55cabd
ewestra <ewestra@rhinofly.nl>
2012-05-10 11:39:08 +0200

    Cleanup of projects and proper publish settings

--- apis/s3/project/Build.scala
+++ apis/s3/project/Build.scala
@@ -4,15 +4,18 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "api-s3"
-    val appVersion      = "1.0"
+  val appName = "api-s3"
+  val appVersion = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "nl.rhinofly" %% "api-utils" % "1.0"
-    )
+  val appDependencies = Seq(
+    "nl.rhinofly" %% "api-utils" % "1.0-SNAPSHOT")
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-       organization := "nl.rhinofly"
-    )
+  val rhinoflyRepo = "Rhinofly Internal Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-snapshot-local"
+
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    organization := "nl.rhinofly",
+    resolvers += rhinoflyRepo,
+    publishTo := Some(rhinoflyRepo),
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"))
 
 }
